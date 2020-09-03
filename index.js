10e6797357559e181ed52fd4f9365d0feb984848
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-09-03 21:03:55 +0700

    feat: Basic command handler

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+const command = require('./commands/command')
+
 client.on('ready', function () {
   console.log(`${client.user.username} is ready!`);
+
+  command(client, 'ping', (message) => {
+    message.channel.send('Pong!')
+  })
 })
 
 client.login(config.token);
