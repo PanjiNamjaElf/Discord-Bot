a6e94773b980d7bdb999733b9a892c7bd86b0b00
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-09-06 15:15:16 +0700

    feat: Ban user command

--- index.js
+++ index.js
@@ -138,6 +138,32 @@ client.on('ready', function () {
 
     message.channel.send(embed)
   })
+
+  command(client, 'ban', (message) => {
+    const { member, mentions } = message
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('BAN_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+
+        targetMember.ban()
+
+        message.channel.send(`${tag} That user has been banned.`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to ban.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
 })
 
 client.login(config.token)
