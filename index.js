7e79db62ecb3be96423121fa16a7eac692981775
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-09-06 08:35:12 +0700

    feat: Send embed server info

--- index.js
+++ index.js
@@ -110,12 +110,32 @@ client.on('ready', function () {
   command(client, 'serverinfo', (message) => {
     const { guild } = message
 
-    const { name, region, memberCount, owner } = guild
+    const { name, region, memberCount, owner, afkTimeout } = guild
     const icon = guild.iconURL()
 
-    console.log(name, region, memberCount, icon)
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Server info for "${name}"`)
+      .setThumbnail(icon)
+      .addFields(
+        {
+          name: 'Region',
+          value: region,
+        },
+        {
+          name: 'Members',
+          value: memberCount,
+        },
+        {
+          name: 'Owner',
+          value: owner.user.tag,
+        },
+        {
+          name: 'AFK Timeout',
+          value: afkTimeout / 60,
+        },
+      )
 
-    console.log(owner.user.tag)
+    message.channel.send(embed)
   })
 })
 
